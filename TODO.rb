344704024a6420955a28a7168873e3e20cb1141e
Stephen Richards <stephen@stephenrichards.eu>
2009-08-17 18:27:51 +0100

    modified TODO

--- TODO.rb
+++ TODO.rb
@@ -1,8 +1,10 @@
 # Still to do for HolidayCalendar project
 # 
-# * instantiate HolidayCalendar object from yaml file
+# * instantiate HolidayCalendar object from yaml file - done
 # * instantiate HolidayCalendar object from a standard configuration
 # * list holidays in general, or for a particular year
 # * implement sunday_to_monday
 # * implement saturday_to_friday
+# * allow deletion of a holiday
+# * allow addition of a holiday
 
