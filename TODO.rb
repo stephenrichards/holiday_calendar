d3fcc37ecc5ecd3c2f185cccfcb6c2309f6d861b
Stephen Richards <stephen@stephenrichards.eu>
2009-08-24 23:25:22 +0100

    change to todo file

--- TODO.rb
+++ TODO.rb
@@ -1,13 +1,12 @@
 # Still to do for HolidayCalendar project
 # 
-# * list holidays in general, or for a particular year  - return as an array of strings
-# * implement sunday_to_monday
-# * implement saturday_to_friday
+
+# * list: generic description of holiday: return array of strings
+# * list_for_year: actual dates: return array of strings.
+# * implement sunday_to_monday           carry_forward_if_sunday
+# * implement saturday_to_friday         take_before_if_saturday
 # * allow deletion of a holiday
-# * allow addition of a holiday
+# * allow addition of a holiday  - done
 
 
-##### test 
-            HolidayCalendar << phs
-            HolidayCalendar << [phs, phs, phs]
 
